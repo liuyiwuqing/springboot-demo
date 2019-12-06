1c04e50eec9dd2583081946c09971a273ea4601c
liuyiwuqing <1520431201@qq.com>
2019-12-06 18:33:26 +0800

    整合oss上传、下载、删除

--- src/main/java/com/lywq/demo/common/utils/OSSClientUtil.java
+++ src/main/java/com/lywq/demo/common/utils/OSSClientUtil.java
@@ -11,10 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
+import java.io.*;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -57,22 +54,6 @@ public class OSSClientUtil {
      * 阿里云API的文件夹名称
      */
     private static String folder;
-    /**
-     * 阿里云API的文件夹名称 [券模块]
-     */
-    private static String couponFolder;
-    /**
-     * 阿里云API的文件夹名称 [图片子模块]
-     */
-    private static String imageFolder;
-    /**
-     * 阿里云API的文件夹名称 [视频子模块]
-     */
-    private static String videoFolder;
-    /**
-     * 阿里云API的文件夹名称 [文件子模块]
-     */
-    private static String textFolder;
 
     /**
      * 获取阿里云OSS客户端对象
@@ -123,7 +104,7 @@ public class OSSClientUtil {
      */
     public static String createFolder(OSSClient ossClient, String bucketName, String folder) {
         // 文件夹名
-        final String keySuffixWithSlash = folder;
+        final String keySuffixWithSlash = folder + "/";
         // 判断文件夹是否存在，不存在则创建
         if (!ossClient.doesObjectExist(bucketName, keySuffixWithSlash)) {
             // 创建文件夹
@@ -151,121 +132,74 @@ public class OSSClientUtil {
     }
 
     /**
-     * 上传文件至OSS
-     *
-     * @param ossClient  oss连接
-     * @param file       上传文件（文件全路径如：D:\\image\\lywq.jpg）
-     * @param bucketName 存储空间
-     * @param folder     模拟文件夹名 如"liuyiwuqing/"
-     * @return String 返回的唯一MD5数字签名
-     */
-    public static String uploadObjectOSS(OSSClient ossClient, File file, String bucketName, String folder) {
-        String resultStr = null;
-        try {
-            // 以输入流的形式上传文件
-            InputStream is = new FileInputStream(file);
-            // 文件名
-            String fileName = file.getName();
-            // 文件大小
-            Long fileSize = file.length();
-            // 创建上传Object的Metadata
-            ObjectMetadata metadata = new ObjectMetadata();
-            // 上传的文件的长度
-            metadata.setContentLength(is.available());
-            // 指定该Object被下载时的网页的缓存行为
-            metadata.setCacheControl("no-cache");
-            // 指定该Object下设置Header
-            metadata.setHeader("Pragma", "no-cache");
-            // 指定该Object被下载时的内容编码格式
-            metadata.setContentEncoding("utf-8");
-            // 文件的MIME，定义文件的类型及网页编码，决定浏览器将以什么形式、什么编码读取文件。如果用户没有指定则根据Key或文件名的扩展名生成，
-            // 如果没有扩展名则填默认值application/octet-stream
-            metadata.setContentType(getContentType(fileName));
-            // 指定该Object被下载时的名称（指示MINME用户代理如何显示附加的文件，打开或下载，及文件名称）
-            metadata.setContentDisposition("filename/filesize=" + fileName + "/" + fileSize + "Byte.");
-            // 上传文件 (上传文件流的形式)
-            PutObjectResult putResult = ossClient.putObject(bucketName, folder + fileName, is, metadata);
-            logger.info("上传文件返回结果：{}", JSON.toJSONString(putResult));
-            // 解析结果
-            resultStr = putResult.getETag();
-        } catch (Exception e) {
-            e.printStackTrace();
-            logger.error("上传阿里云OSS服务器异常." + e.getMessage(), e);
-        }
-        return resultStr;
-    }
-
-    /**
-     * 上传图片至OSS
-     *
-     * @param ossClient oss连接
-     * @param file      上传文件（文件全路径如：D:\\image\\lywq.jpg）
-     *                  存储空间
-     *                  //     * @param folder
-     *                  模拟文件夹名 如"image/"
-     * @return String 返回的唯一MD5数字签名
-     */
-    public static String uploadImgToOSS(OSSClient ossClient, MultipartFile file) {
-//		String resultStr = null;
-        //String url = null;
-        String downFileName = null;
-        try {
-            // 以输入流的形式上传文件
-            InputStream is = file.getInputStream();
-            // 文件名
-            String fileName = file.getOriginalFilename();
-            // 文件类型
-            String type = getContentType(fileName);
-            if (!type.equalsIgnoreCase("image")){
-                return "上传类型不是图片";
-            }
-            // 文件大小
-            Long fileSize = file.getSize();
-            if (fileSize > 1024 * 1024 * 2) {
-                return "上传文件大于2M";
-            }
-            // 创建上传Object的Metadata
-            ObjectMetadata metadata = new ObjectMetadata();
-            // 上传的文件的长度
-            metadata.setContentLength(is.available());
-            // 指定该Object被下载时的网页的缓存行为
-            metadata.setCacheControl("no-cache");
-            // 指定该Object下设置Header
-            metadata.setHeader("Pragma", "no-cache");
-            // 指定该Object被下载时的内容编码格式
-            metadata.setContentEncoding("utf-8");
-            // 文件的MIME，定义文件的类型及网页编码，决定浏览器将以什么形式、什么编码读取文件。如果用户没有指定则根据Key或文件名的扩展名生成，
-            // 如果没有扩展名则填默认值application/octet-stream
-            metadata.setContentType(getContentType(fileName));
-            // 指定该Object被下载时的名称（指示MINME用户代理如何显示附加的文件，打开或下载，及文件名称）
-            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS");
-            String time = df.format(new Date());
-            StringBuilder StringBuilder = new StringBuilder();
-            String suffix = fileName.substring(fileName.lastIndexOf(".") + 1);
-            String prefix = fileName.substring(0, fileName.lastIndexOf("."));
-            downFileName = StringBuilder.append(prefix).append("_").append(time).append(".").append(suffix).toString();
-            metadata.setContentDisposition("filename/filesize=" + downFileName + "/" + fileSize + "Byte.");
-            // 上传文件 (上传文件流的形式)
-            PutObjectResult putResult = ossClient.putObject(bucketName, imageFolder + downFileName, is, metadata);
-            logger.info("上传文件返回结果：{}", JSON.toJSONString(putResult));
-        } catch (Exception e) {
-            e.printStackTrace();
-            logger.error("上传阿里云OSS服务器异常." + e.getMessage(), e);
-        }
-        return imageFolder + downFileName;
-    }
-
-
-    /**
-     * 上传视频到oss
+     * 根据名称删除
      *
      * @param ossClient
-     * @param file
-     * @return
+     * @param fileName  全路径名称 如“upload/images/lywq_2019-12-06 16:34:51:060.jpg”
      */
-    public static String uploadVideoToOSS(OSSClient ossClient, MultipartFile file) {
-        //String url = null;
-        String downFileName = null;
+    public static void delete(OSSClient ossClient, String fileName) {
+        // 根据objectName删除文件
+        ossClient.deleteObject(bucketName, fileName);
+        logger.info("删除" + bucketName + "下的文件" + fileName + "成功");
+    }
+
+//    /**
+//     * 上传文件至OSS
+//     *
+//     * @param ossClient oss连接
+//     * @param file      上传文件（文件全路径如：D:\\image\\lywq.jpg）
+//     * @return String 返回的唯一MD5数字签名
+//     */
+//    public static String uploadObjectOSS(OSSClient ossClient, File file) {
+//        String resultStr = null;
+//        try {
+//            // 以输入流的形式上传文件
+//            InputStream is = new FileInputStream(file);
+//            // 文件名
+//            String fileName = file.getName();
+//            // 文件大小
+//            Long fileSize = file.length();
+//            // 创建上传Object的Metadata
+//            ObjectMetadata metadata = new ObjectMetadata();
+//            // 上传的文件的长度
+//            metadata.setContentLength(is.available());
+//            // 指定该Object被下载时的网页的缓存行为
+//            metadata.setCacheControl("no-cache");
+//            // 指定该Object下设置Header
+//            metadata.setHeader("Pragma", "no-cache");
+//            // 指定该Object被下载时的内容编码格式
+//            metadata.setContentEncoding("utf-8");
+//            // 文件的MIME，定义文件的类型及网页编码，决定浏览器将以什么形式、什么编码读取文件。如果用户没有指定则根据Key或文件名的扩展名生成，
+//            // 如果没有扩展名则填默认值application/octet-stream
+//            metadata.setContentType(getContentType(fileName));
+//            // 指定该Object被下载时的名称（指示MINME用户代理如何显示附加的文件，打开或下载，及文件名称）
+//            metadata.setContentDisposition("filename/filesize=" + fileName + "/" + fileSize + "Byte.");
+//            // 上传文件 (上传文件流的形式)
+//            PutObjectResult putResult = ossClient.putObject(bucketName, folder + fileName, is, metadata);
+//            logger.info("上传文件返回结果：{}", JSON.toJSONString(putResult));
+//            // 解析结果
+//            resultStr = putResult.getETag();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            logger.error("上传阿里云OSS服务器异常." + e.getMessage(), e);
+//        }
+//        return resultStr;
+//    }
+
+    /**
+     * 上传文件至OSS
+     *
+     * @param ossClient oss连接
+     * @param file      上传文件（文件全路径如：D:\\images\\lywq.jpg）
+     *                  //@param bucketName 如"liuyiwuqing"
+     *                  //@param folder 如"upload/"
+     *                  子文件夹 如"images/"
+     * @return String 返回的唯一MD5数字签名
+     */
+    public static String uploadFileToOSS(OSSClient ossClient, MultipartFile file) {
+        String fileFolder = null; // 定义文件夹
+        Long fileSize = null; // 定义文件大小
+        String downFileName = null; // 定义文件名
         try {
             // 以输入流的形式上传文件
             InputStream is = file.getInputStream();
@@ -273,13 +207,37 @@ public class OSSClientUtil {
             String fileName = file.getOriginalFilename();
             // 文件类型
             String type = getContentType(fileName);
-            if (!type.equalsIgnoreCase("video")){
-                return "上传类型不是视频";
+            if (type.equalsIgnoreCase("image")) {
+                fileFolder = "images/";
+                // 文件大小
+                fileSize = file.getSize();
+                if (fileSize > 1024 * 1024 * 2) {
+                    return "上传文件大于2M";
+                }
             }
-            // 文件大小
-            Long fileSize = file.getSize();
-            if (fileSize > 1024 * 1024 * 50) {
-                return "上传文件大于50M";
+            if (type.equalsIgnoreCase("video")) {
+                fileFolder = "videos/";
+                // 文件大小
+                fileSize = file.getSize();
+                if (fileSize > 1024 * 1024 * 50) {
+                    return "上传文件大于50M";
+                }
+            }
+            if (type.equalsIgnoreCase("text")) {
+                fileFolder = "texts/";
+                // 文件大小
+                fileSize = file.getSize();
+                if (fileSize > 1024 * 1024 * 5) {
+                    return "上传文件大于5M";
+                }
+            }
+            if (type.equalsIgnoreCase("package")) {
+                fileFolder = "packages/";
+                // 文件大小
+                fileSize = file.getSize();
+                if (fileSize > 1024 * 1024 * 100) {
+                    return "上传文件大于100M";
+                }
             }
             // 创建上传Object的Metadata
             ObjectMetadata metadata = new ObjectMetadata();
@@ -303,13 +261,41 @@ public class OSSClientUtil {
             downFileName = StringBuilder.append(prefix).append("_").append(time).append(".").append(suffix).toString();
             metadata.setContentDisposition("filename/filesize=" + downFileName + "/" + fileSize + "Byte.");
             // 上传文件 (上传文件流的形式)
-            PutObjectResult putResult = ossClient.putObject(bucketName, videoFolder + downFileName, is, metadata);
+            PutObjectResult putResult = ossClient.putObject(bucketName, folder + fileFolder + timeFolder() + downFileName, is, metadata);
             logger.info("上传文件返回结果：{}", JSON.toJSONString(putResult));
         } catch (Exception e) {
             e.printStackTrace();
             logger.error("上传阿里云OSS服务器异常." + e.getMessage(), e);
         }
-        return videoFolder + downFileName;
+        return folder + fileFolder + timeFolder() + downFileName;
+    }
+
+    /**
+     * 下载文件
+     * @param ossClient
+     * @param os
+     * @param fileName
+     * @throws IOException
+     */
+    public static void exportOssFile(OSSClient ossClient, OutputStream os, String fileName) throws IOException {
+        // ossObject包含文件所在的存储空间名称、文件名称、文件元信息以及一个输入流。
+        OSSObject ossObject = ossClient.getObject(bucketName, fileName);
+        // 读取文件内容。
+        BufferedInputStream in = new BufferedInputStream(ossObject.getObjectContent());
+        BufferedOutputStream out = new BufferedOutputStream(os);
+        byte[] buffer = new byte[1024];
+        int lenght = 0;
+        while ((lenght = in.read(buffer)) != -1) {
+            out.write(buffer, 0, lenght);
+        }
+        if (out != null) {
+            logger.info("下载" + bucketName + "下的文件" + fileName + "成功");
+            out.flush();
+            out.close();
+        }
+        if (in != null) {
+            in.close();
+        }
     }
 
 
@@ -322,6 +308,7 @@ public class OSSClientUtil {
     public static String getContentType(String fileName) {
         // 文件的后缀名
         String fileExtension = fileName.substring(fileName.lastIndexOf("."));
+        // 判断文件类型为图片
         if (".bmp".equalsIgnoreCase(fileExtension)
                 || ".jpg".equalsIgnoreCase(fileExtension)
                 || ".png".equalsIgnoreCase(fileExtension)
@@ -329,6 +316,7 @@ public class OSSClientUtil {
                 || ".jpeg".equalsIgnoreCase(fileExtension)) {
             return "image";
         }
+        // 判断文件类型为视频
         if (".mp4".equalsIgnoreCase(fileExtension)
                 || ".flv".equalsIgnoreCase(fileExtension)
                 || ".mp4".equalsIgnoreCase(fileExtension)
@@ -338,15 +326,23 @@ public class OSSClientUtil {
                 || ".ogg".equalsIgnoreCase(fileExtension)) {
             return "video";
         }
+        // 判断文件类型为文本
         if (".txt".equalsIgnoreCase(fileExtension)
-                ||".ppt".equalsIgnoreCase(fileExtension)
+                || ".ppt".equalsIgnoreCase(fileExtension)
                 || "pptx".equalsIgnoreCase(fileExtension)
-                ||".doc".equalsIgnoreCase(fileExtension)
+                || ".doc".equalsIgnoreCase(fileExtension)
                 || "docx".equalsIgnoreCase(fileExtension)
                 || ".xml".equalsIgnoreCase(fileExtension)
                 || ".vsd".equalsIgnoreCase(fileExtension)
                 || ".html".equalsIgnoreCase(fileExtension)) {
             return "text";
+        }
+        // 判断文件类型为压缩包
+        if (".rar".equalsIgnoreCase(fileExtension)
+                || ".zip".equalsIgnoreCase(fileExtension)
+                || "gz".equalsIgnoreCase(fileExtension)
+                || ".7z".equalsIgnoreCase(fileExtension)) {
+            return "package";
         }
         // 默认返回类型
         return "";
@@ -391,29 +387,39 @@ public class OSSClientUtil {
         return null;
     }
 
-
     /**
-     * 下载图片
+     * 当前日期当文件夹名
      *
-     * @param fileName
      * @return
      */
-    public static OSSObject downLoadImage(String fileName) {
-        OSSClient ossClient = OSSClientUtil.getOSSClient();
-        Date expiration = new Date(new Date().getTime() + 3600 * 1000);
-        GeneratePresignedUrlRequest request = new GeneratePresignedUrlRequest(bucketName, fileName, HttpMethod.GET);
-        // 设置过期时间。
-        request.setExpiration(expiration);
-        // 生成签名URL（HTTP GET请求）。
-        URL signedUrl = ossClient.generatePresignedUrl(request);
-        // 使用签名URL发送请求。
-        Map<String, String> customHeaders = new HashMap<String, String>();
-        // 添加GetObject请求头。
-        //customHeaders.put("Range", "bytes=100-1000");
-        OSSObject object = ossClient.getObject(signedUrl, customHeaders);
-        return object;
+
+    public static String timeFolder() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd/");
+        String str = sdf.format(new Date());
+        return str;
     }
 
+//    /**
+//     * 下载文件
+//     *
+//     * @param ossClient
+//     * @param fileName
+//     * @return
+//     */
+//    public static OSSObject downLoadFile(OSSClient ossClient, String fileName) {
+//        Date expiration = new Date(new Date().getTime() + 3600 * 1000);
+//        GeneratePresignedUrlRequest request = new GeneratePresignedUrlRequest(bucketName, fileName, HttpMethod.GET);
+//        // 设置过期时间。
+//        request.setExpiration(expiration);
+//        // 生成签名URL（HTTP GET请求）。
+//        URL signedUrl = ossClient.generatePresignedUrl(request);
+//        // 使用签名URL发送请求。
+//        Map<String, String> customHeaders = new HashMap<String, String>();
+//        // 添加GetObject请求头。
+//        //customHeaders.put("Range", "bytes=100-1000");
+//        OSSObject object = ossClient.getObject(signedUrl, customHeaders);
+//        return object;
+//    }
 
     @Value("${aliyun.accessKeyId}")
     public void setAccessKeyId(String accessKeyId) {
@@ -440,27 +446,6 @@ public class OSSClientUtil {
         OSSClientUtil.folder = folder;
     }
 
-    @Value("${aliyun.oss.subfolder.image.folder}")
-    public static void setImageFolder(String imageFolder) {
-        OSSClientUtil.imageFolder = imageFolder;
-    }
-
-    @Value("${aliyun.oss.subfolder.video.folder}")
-    public static void setVideoFolder(String videoFolder) {
-        OSSClientUtil.videoFolder = videoFolder;
-    }
-
-    @Value("${aliyun.oss.subfolder.text.folder}")
-    public static void setTextFolder(String textFolder) {
-        OSSClientUtil.textFolder = textFolder;
-    }
-
-    @Value("${aliyun.oss.coupon.image.folder}")
-    public void setCouponFolder(String couponFolder) {
-        OSSClientUtil.couponFolder = couponFolder;
-    }
-
-
     public static String getAccessKeyId() {
         return accessKeyId;
     }
@@ -480,21 +465,4 @@ public class OSSClientUtil {
     public static String getFolder() {
         return folder;
     }
-
-    public static String getImageFolder() {
-        return imageFolder;
-    }
-
-    public static String getVideoFolder() {
-        return videoFolder;
-    }
-
-    public static String getTextFolder() {
-        return textFolder;
-    }
-
-    public static String getCouponFolder() {
-        return couponFolder;
-    }
-
 }
--- src/main/java/com/lywq/demo/modular/uploadModular/controller/UploadOSSController.java
+++ src/main/java/com/lywq/demo/modular/uploadModular/controller/UploadOSSController.java
@@ -1,18 +1,18 @@
 package com.lywq.demo.modular.uploadModular.controller;
 
 import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.model.OSSObject;
 import com.lywq.demo.common.modle.RetResponse;
 import com.lywq.demo.common.modle.RetResult;
 import com.lywq.demo.common.utils.OSSClientUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 
 /**
  * @author 王恩典
@@ -26,33 +26,21 @@ import java.io.File;
 @RestController
 public class UploadOSSController {
 
-    @ApiOperation(value = "图片上传", notes = "图片上传")
-    @PostMapping(value = "/uploadImg")
-    public RetResult<String> uploadImg(@RequestParam("file") MultipartFile file) {
+    @ApiOperation(value = "文件上传", notes = "文件上传")
+    @PostMapping(value = "/uploadFile")
+    public RetResult<String> uploadFile(@RequestParam("file") MultipartFile file) {
         // 获取实例
         OSSClient ossClient = OSSClientUtil.getOSSClient();
-        // 上传图片
-        String uploadImgOSS = OSSClientUtil.uploadImgToOSS(ossClient, file);
+        // 上传文件
+        String uploadImgOSS = OSSClientUtil.uploadFileToOSS(ossClient, file);
         // url地址
         String privateUrl = OSSClientUtil.getPrivateUrl(ossClient, uploadImgOSS);
         return RetResponse.makeOKRsp(privateUrl);
     }
 
-    @ApiOperation(value = "视频上传", notes = "视频上传")
-    @PostMapping(value = "/uploadVideo")
-    public RetResult<String> uploadVideo(@RequestParam("file") MultipartFile file) {
-        // 获取实例
-        OSSClient ossClient = OSSClientUtil.getOSSClient();
-        // 上传视频
-        String uploadVideoOSS = OSSClientUtil.uploadVideoToOSS(ossClient, file);
-        // url地址
-        String privateUrl = OSSClientUtil.getPrivateUrl(ossClient, uploadVideoOSS);
-        return RetResponse.makeOKRsp(privateUrl);
-    }
-
     @ApiOperation(value = "创建文件夹", notes = "创建文件夹")
     @PostMapping(value = "/mkdir")
-    public RetResult<String>  mkdir(String bucketName, String folder){
+    public RetResult<String> mkdir(String bucketName, String folder){
         // 获取实例
         OSSClient ossClient = OSSClientUtil.getOSSClient();
         // 创建文件夹
@@ -60,5 +48,35 @@ public class UploadOSSController {
         return RetResponse.makeOKRsp(fileDir);
     }
 
+    @ApiOperation(value = "删除文件", notes = "删除文件")
+    @GetMapping(value = "deleteFile")
+    public RetResult<String> deleteFile(String fileName){
+        // 获取实例
+        OSSClient ossClient = OSSClientUtil.getOSSClient();
+        OSSClientUtil.delete(ossClient,fileName);
+        return RetResponse.makeOKRsp("删除成功");
+    }
+
+//    @ApiOperation(value = "下载文件", notes = "下载文件")
+//    @GetMapping(value = "downLoadFile")
+//    public RetResult<String> downLoadFile(String fileName){
+//        // 获取实例
+//        OSSClient ossClient = OSSClientUtil.getOSSClient();
+//        OSSObject object = OSSClientUtil.downLoadFile(ossClient,fileName);
+//        System.out.println(object);
+//        return RetResponse.makeOKRsp("下载成功");
+//    }
+
+    @ApiOperation(value = "下载文件", notes = "下载文件")
+    @GetMapping("download")
+    public RetResult<String> download(@RequestParam("fileName") String fileName, HttpServletResponse response) throws IOException {
+        // 获取实例
+        OSSClient ossClient = OSSClientUtil.getOSSClient();
+        //通知浏览器以附件形式下载
+        response.setHeader("Content-Disposition",
+                "attachment;filename=" + new String(fileName.getBytes(), "ISO-8859-1"));
+        OSSClientUtil.exportOssFile(ossClient,response.getOutputStream(),fileName);
+        return RetResponse.makeOKRsp("下载成功");
+    }
 
 }
